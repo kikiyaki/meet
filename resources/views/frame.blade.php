7577356356f1cac576082a138afa822d272bcd41
kikiyaki <krl9628@gmail.com>
2019-04-24 00:25:05 +0300

    asset src in frame

--- resources/views/frame.blade.php
+++ resources/views/frame.blade.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="js/app.js" charset="utf-8"></script>
+    <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
 
     <!-- Styles -->
     <style>
